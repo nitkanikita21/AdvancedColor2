2e89f0f59f0adc250824fe4390a7312848673511
nitkanikita21 <waleryvvs1@gmail.com>
2022-04-21 18:26:45 +0300

    Publishing to Jitpack

--- AdvancedColor2-plugin-spigot/src/main/java/com/nitkanikita/advancedcolor2/spigot/SpigotMain.java
+++ AdvancedColor2-plugin-spigot/src/main/java/com/nitkanikita/advancedcolor2/spigot/SpigotMain.java
@@ -1,9 +1,5 @@
 package com.nitkanikita.advancedcolor2.spigot;
 
-import com.nitkanikita.advancedcolor2.api.CustomCharacter;
-import com.nitkanikita.advancedcolor2.api.CustomColor;
-import com.nitkanikita.advancedcolor2.api.CustomText;
-import com.nitkanikita.advancedcolor2.api.builders.GradientTextBuilder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,17 +16,6 @@ public class SpigotMain extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("advancedcolor2").setExecutor((sender, command, label, args) -> {
-
-            String minecraftString = GradientTextBuilder.get()
-                    .addText(CustomText.text("Hello! Its my cool text. Do you like my gradient?", CustomCharacter.Code.ITALIC.bit() | CustomCharacter.Code.BOLD.bit()))
-                    .addColor(CustomColor.of(Color.MAGENTA))
-                    .addColor(CustomColor.of(Color.GREEN))
-                    .addColor(CustomColor.of(Color.BLUE))
-                    .smooth(0.01)
-                    .generate()
-                    .getMinecraftString();
-
-            sender.sendMessage(minecraftString);
 
             return true;
         });
